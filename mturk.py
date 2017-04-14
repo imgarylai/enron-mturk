70c1d8a4e560585d0a0009658eee235d721ca7ef
Gary Lai <garylai1990@gmail.com>
2017-04-14 14:02:04 -0400

    Wrap api of getting all assignments page by page

--- mturk.py
+++ mturk.py
@@ -1,3 +1,4 @@
+import itertools
 import yaml
 from boto.mturk.connection import MTurkConnection, MTurkRequestError
 from mturk_tmpl import MturkTmpl, ReceiversTooMuch, TokenLength
@@ -22,6 +23,17 @@ class Mturk():
 
     def get_hits(self):
         return self.mturk.get_all_hits()
+
+    def get_all_assignments(self, hit_id):
+        page_size = 100
+        assignments = self.mturk.get_assignments(hit_id, page_size = page_size)
+        total_records = int(assignments.TotalNumResults)
+        get_page_assignments = lambda page: self.mturk.get_assignments(hit_id,
+            page_size=page_size,
+            page_number=page)
+        page_nums = self.mturk._get_pages(page_size, total_records)
+        assignments_sets = itertools.imap(get_page_assignments, page_nums)
+        return itertools.chain.from_iterable(assignments_sets)
 
     def remove_old_hits(self):
         # Disable old hits.
